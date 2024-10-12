e2c49d9fc83a529b96d8bb86d1b67a906c28fd92
katg999 <katex911@gmail.com>
2024-10-13 02:40:06 +0300

    React Composition Using the Children Prop

--- src/App.js
+++ src/App.js
@@ -51,28 +51,32 @@ const average = (arr) =>
 	arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
 export default function App() {
+	const [movies, setMovies] = useState(tempMovieData);
 	return (
 		<>
-			<NavBar />
-			<Main />
+			<NavBar>
+				<Logo />
+				<Search />
+				<NumResults movies={movies} />
+			</NavBar>
+			<Main>
+				<ListBox>
+					<MovieList movies={movies} />
+				</ListBox>
+				<WatchedBox />
+			</Main>
 		</>
 	);
 }
 
-function NavBar() {
-	return (
-		<nav className='nav-bar'>
-			<Logo />
-			<Search />
-			<NumResults />
-		</nav>
-	);
+function NavBar({ children }) {
+	return <nav className='nav-bar'>{children}</nav>;
 }
 
-function NumResults() {
+function NumResults({ movies }) {
 	return (
 		<p className='num-results'>
-			Found <strong>X</strong> results
+			Found <strong>{movies.length}</strong> results
 		</p>
 	);
 }
@@ -99,16 +103,11 @@ function Search() {
 	);
 }
 
-function Main() {
-	return (
-		<main className='main'>
-			<ListBox />
-			<WatchedBox />
-		</main>
-	);
+function Main({ children }) {
+	return <main className='main'>{children}</main>;
 }
 
-function ListBox() {
+function ListBox({ children }) {
 	const [isOpen1, setIsOpen1] = useState(true);
 
 	return (
@@ -119,13 +118,12 @@ function ListBox() {
 			>
 				{isOpen1 ? '–' : '+'}
 			</button>
-			{isOpen1 && <MovieList />}
+			{isOpen1 && children}
 		</div>
 	);
 }
 
-function MovieList() {
-	const [movies, setMovies] = useState(tempMovieData);
+function MovieList({ movies }) {
 	return (
 		<ul className='list'>
 			{movies?.map((movie) => (
